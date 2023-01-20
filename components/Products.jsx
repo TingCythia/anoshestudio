39c6af4f9adcc179cade709792c8126d83fa5c01
TingCythia <91254839+TingCythia@users.noreply.github.com>
2023-01-20 14:42:05 +0100

    create new store component

---
+++ components/Products.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Link from 'next/link';
+import { urlFor } from '../lib/client';
+
+function Products({ product: { image, name, slug, price } }) {
+    return (
+        <div>
+            <Link href={`/product/${slug.current}`}>
+                <div className='store-product-card'>
+                    <img
+                        src={urlFor(image && image[0])}
+                        width={250}
+                        height={250}
+                        className="product-image"
+                    />
+                    <p className='product-name'>{name}</p>
+                    <p className='product-price'>€{price}</p>
+                </div>
+            </Link>
+        </div>
+    )
+}
+
+export default Products
---
+++ pages/store.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { client } from '../lib/client';
+import { Products } from '../components';
+import { useStateContext } from '../context/StateContext';
+
+const Store = ({ products }) => {
+    const { onAdd } = useStateContext();
+    return (
+        <>
+            <div className='store-products-container'>
+                {products?.map((product) =>
+                    <div>
+                        <Products
+                            key={product._id}
+                            product={product}
+                        />
+                        <button
+                            type='button'
+                            className='add-to-cart'
+                            onClick={() => onAdd(product, qty)}
+                        >Add to cart
+                        </button>
+                    </div>
+                )}
+
+            </div>
+        </>
+    )
+}
+
+export default Store
+
+export const getServerSideProps = async () => {
+    const query = '*[_type == "product"]';
+    const products = await client.fetch(query);
+
+
+    return {
+        props: { products }
+    }
+}
