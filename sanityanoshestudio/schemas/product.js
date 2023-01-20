0b40056e31a2641a2861112fb047c131c501cdb7
TingCythia <91254839+TingCythia@users.noreply.github.com>
2023-01-20 14:12:56 +0100

    update category

--- sanityanoshestudio/schemas/product.js
+++ sanityanoshestudio/schemas/product.js
@@ -35,6 +35,11 @@ export default {
             name: 'details',
             title: 'Details',
             type: 'string',
-        }
+        },
+        {
+            name: 'category',
+            title: 'Category',
+            type: 'string',
+        },
     ]
 }
